b3462b5970f203ccad1bdd2e225cf34a298c1d1e
SHWplus <2066936526@qq.com>
2026-09-14 06:52:34 +0000

    use absolute qpos actions with quantile normalization

--- src/lbm/dataloader/custom/datasets/robocoin.py
+++ src/lbm/dataloader/custom/datasets/robocoin.py
@@ -2,4 +2,4 @@
 from ._lerobot_bind import bind_lerobot
 from lbm.action_space import unimanual_joint
 # Common camera names across RoboCOIN; missing streams are handled by scanner.
-NAME,SPEC,scan,read_vectors,read_frames=bind_lerobot('robocoin','franka',('observation.images.cam_head_rgb','observation.images.cam_left_wrist_rgb','observation.images.cam_right_wrist_rgb'),8,8,30.0,kind='lerobot',action_space=unimanual_joint())
+NAME,SPEC,scan,read_vectors,read_frames=bind_lerobot('robocoin','franka',('observation.images.cam_head_rgb','observation.images.cam_left_wrist_rgb','observation.images.cam_right_wrist_rgb'),8,8,30.0,kind='lerobot',action_space=unimanual_joint(rep="abs"))
